c15b5710aa8f0fba867a053c8bc4f2e1b6c42ef4
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-05-17 02:22:04 +0700

    add profile button

--- hooks/action-button-organization_users.php
+++ hooks/action-button-organization_users.php
@@ -1,5 +1,6 @@
 <?php
 
 return '
+<a href="'.routeTo('default/profile',['user_id'=>$data->user_id]).'" class="btn btn-sm btn-info">Profile</a>
 <a href="'.routeTo('crud/index',['table'=>'organization_user_positions','filter'=>['organization_user_id' => $data->id]]).'" class="btn btn-sm btn-info"><i class="fas fa-users"></i> '.__('organization.label.positions').'</a>
 ';
