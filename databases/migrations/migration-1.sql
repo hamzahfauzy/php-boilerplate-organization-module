2dc20c8a052318cc42477becfafaeb9eca03599a
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-09-25 12:31:53 +0700

    change role default

--- databases/migrations/migration-1.sql
+++ databases/migrations/migration-1.sql
@@ -10,7 +10,7 @@ CREATE TABLE organization_users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     organization_id INT NOT NULL,
     user_id INT NOT NULL,
-    role VARCHAR(100) NOT NULL,
+    role VARCHAR(100) DEFAULT NULL,
     CONSTRAINT fk_organization_users_organization_id FOREIGN KEY (organization_id) REFERENCES organizations(id) ON DELETE CASCADE,
     CONSTRAINT fk_organization_users_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
